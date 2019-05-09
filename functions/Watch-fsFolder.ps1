2081d0b184ff50a0de31dc892dc753d4512b7b1a
Mats Tegbjer <mats.tegbjer@scanfil.com>
2019-05-09 18:38:10 +0200

    Update Watch-fsFolder.ps1

--- functions/Watch-fsFolder.ps1
+++ functions/Watch-fsFolder.ps1
@@ -55,7 +55,7 @@ function Watch-fsFolder
         [Parameter(Mandatory=$false,
                    Position=2)]
         [ValidateScript({  Test-Path -Path $_ -PathType Container  })]
-        [string]$Destination = $env:temp,
+        [string]$Destination,
 
         [Parameter(Mandatory=$false,
                    Position=3)]
@@ -63,7 +63,7 @@ function Watch-fsFolder
 
         [Parameter(Mandatory=$false,
                    Position=4)]
-        [string]$LogFile = "Watch-Folder.log",
+        [string]$LogFile = "Watch-Folder.csv",
 
         [Parameter(HelpMessage = 'Watch for changes')]
         [switch]$includeChanged = $true,
@@ -79,42 +79,108 @@ function Watch-fsFolder
 
     )
 		   
-	if($location -eq ""){
-		$location = get-location
-	}
-	
-	$watcher = New-Object System.IO.FileSystemWatcher
-	$watcher.Path = $location
-	$watcher.IncludeSubdirectories = $includeSubdirectories
-	$watcher.EnableRaisingEvents = $false
-	$watcher.NotifyFilter = [System.IO.NotifyFilters]::LastWrite -bor [System.IO.NotifyFilters]::FileName
-	
-	$conditions = 0
-	if($includeChanged){
-		$conditions = [System.IO.WatcherChangeTypes]::Changed 
-	}
+    Begin
+    {
+        if(!$Destination)
+        {
+            $LogFile = Join-Path -Path (Get-Location) -ChildPath $LogFile
+            Start-Log -Log $LogFile
+        }
+        else
+        {
+            Start-Log -Log (Join-Path -Path $Destination -ChildPath $LogFile)
+        }
+        #Write-Host "The log file was created at: [$ScriptLogFilePath]"
+        #Write-Log -Message "The log file was created at: [$ScriptLogFilePath]" -LogLevel 1
 
-	if($includeRenamed){
-		$conditions = $conditions -bOr [System.IO.WatcherChangeTypes]::Renamed
-	}
-
-	if($includeCreated){
-		$conditions = $conditions -bOr [System.IO.WatcherChangeTypes]::Created 
-	}
-
-	if($includeDeleted){
-		$conditions = $conditions -bOr [System.IO.WatcherChangeTypes]::Deleted
-	}
+        $fsw = New-Object System.IO.FileSystemWatcher
+	    $fsw.Path = $Folder
+        $fsw.Filter = $Filter
+	    $fsw.IncludeSubdirectories = $includeSubdirectories
+	    $fsw.EnableRaisingEvents = $false
+	    $fsw.NotifyFilter = [System.IO.NotifyFilters]::LastWrite -bor [System.IO.NotifyFilters]::FileName
 	
-	while($TRUE){
-		$result = $watcher.WaitForChanged($conditions, 1000);
-		if($result.TimedOut){
-			continue;
-		}
-		$filepath = [System.IO.Path]::Combine($location, $result.Name)
-		New-Object Object |
-          Add-Member NoteProperty Path $filepath -passThru | 
-          Add-Member NoteProperty Operation $result.ChangeType.ToString() -passThru | 
-          write-output
-	}
+	    $conditions = 0
+	    if($includeChanged)
+        {
+		    $conditions = [System.IO.WatcherChangeTypes]::Changed 
+	    }
+
+	    if($includeRenamed)
+        {
+		    $conditions = $conditions -bOr [System.IO.WatcherChangeTypes]::Renamed
+	    }
+
+	    if($includeCreated)
+        {
+		    $conditions = $conditions -bOr [System.IO.WatcherChangeTypes]::Created 
+	    }
+
+	    if($includeDeleted)
+        {
+		    $conditions = $conditions -bOr [System.IO.WatcherChangeTypes]::Deleted
+	    }
+    }
+    Process
+    {
+	    while($true){
+		    $Event = $fsw.WaitForChanged($conditions, 1000);
+		    if($Event.TimedOut){
+                continue;
+		    }
+		    $Timestamp = Get-Date -UFormat '+%Y-%m-%dT%H%M%SZ'
+            $SourceName = $Event.Name
+            $SourceFullPath = [System.IO.Path]::Combine($Folder, $SourceName)
+
+            switch ($Event.ChangeType)
+            {
+                'Changed'
+                {
+
+                    Try
+                    {
+                        if(!$Destination)
+                        {
+                            break;
+                        }
+                        $DestinationName = $Timestamp + "-" + $SourceName
+                        $DestinationFullPath = [System.IO.Path]::Combine($Destination, $DestinationName)
+                        $LastWrite = Get-ChildItem $Destination -Recurse -Filter "*$SourceName" -Exclude $LogFile | sort LastWriteTime | select -last 1
+                        if (!$LastWrite)
+                        {
+                        }
+                        elseif(!(Compare-Object -ReferenceObject @($(Get-Content $LastWrite)) -DifferenceObject @($(Get-Content $SourceFullPath)) ) )
+                        {
+                            Throw "Source is a perfect match with the last written file. No actions will be taken."
+                        }
+
+                        Copy-Item -Path $SourceFullPath -Destination $DestinationFullPath
+                        #Write-Log -Message "Successfully copied the file." -Severity Information
+
+                    }
+                    catch
+                    {
+                        #Write-Host "Exception Message: $($_.Exception.Message)" -ForegroundColor Red
+                    }
+
+                }
+                'Created' { }
+                'Deleted' { }
+                'Renamed' { }
+            }
+            #Write-Log -Message Hej -Severity Information
+		    New-Object Object |
+                Add-Member -MemberType NoteProperty -Name Time (Get-Date -Format s) -passThru | 
+                Add-Member -MemberType NoteProperty -Name Operation $Event.ChangeType.ToString() -passThru | 
+                Add-Member -MemberType NoteProperty -Name Source $SourceFullPath -passThru |
+              Write-Output
+            
+            #Write-Log -Message "The log file was created at: [$ScriptLogFilePath]" -LogLevel 1
+	    }
+    }
+    End
+    {
+    }
 }
+
+Watch-fsFolder -Folder C:\watch -Destination "C:\dest"
